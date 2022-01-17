58d356e8e30a427aa0f7be1dd62c7fb6c651b26a
Rudolf Klikh <rklik@softserveinc.com>
2022-01-17 13:12:09 +0100

    Section 2 - Solving the Problems

---
+++ lesson2.js
@@ -0,0 +1,31 @@
+function findCommonItem(array1, array2) {
+  // Second Solution. would be :
+  //   const findCommonArr = [...array1 ,...array2].filter((item, index) => [...array1 ,...array2].indexOf(item) !== index); // O(n);
+  //   return findCommonArr.length > 0 ? true : false;
+
+  //   // First Solution , would be :
+  //   for (let i = 0; i < array1.length; i++) { // O(n)
+  //     if (array2.includes(array1[i])) { // O(n)
+  //       return true;
+  //     }
+  //   };
+  //   return false; // O(n^2);
+}
+
+// console.log(findCommonItem(["a", "b", "c", "x"], ["z", "y", "i"]));
+// console.log(findCommonItem(["a", "b", "c", "x"], ["z", "y", "x"]));
+
+
+// function hasPairWithSum2(arr, sum) {
+//     const mySet = new Set();
+    
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log(mySet, 'SET');
+//         if (mySet.has(arr[i])) {
+//             return true;
+//         }
+//         mySet.add(sum - arr[i]);
+//     };
+//     return false;
+// }
+// console.log(hasPairWithSum2([6,4,3,2,1,7], 9));
