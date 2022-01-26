20429bad7d6488bd2c57590757a0c02c1fbdd269
Rudolf Klikh <rklik@softserveinc.com>
2022-01-26 09:25:09 +0100

    lesson-9 graph

--- lesson6.js
+++ lesson6.js
@@ -1,9 +1,9 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
 // class Stack {
 //     constructor() {
@@ -49,45 +49,45 @@ class Node {
 // myStack.peek();
 // console.log(myStack);
 
-class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
-  }
+// class Queue {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.length = 0;
+//   }
 
-  peek() {
-    return this.first;
-  }
+//   peek() {
+//     return this.first;
+//   }
 
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    this.length++;
-  }
+//   enqueue(value) {
+//     const newNode = new Node(value);
+//     if (this.length === 0) {
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       this.last.next = newNode;
+//       this.last = newNode;
+//     }
+//     this.length++;
+//   }
 
-  dequeue() {
-    if (!this.first) {
-      return null;
-    }
-    if (this.first === this.last) {
-      this.last === null;
-    }
+//   dequeue() {
+//     if (!this.first) {
+//       return null;
+//     }
+//     if (this.first === this.last) {
+//       this.last === null;
+//     }
 
-    this.first = this.first.next;
-    this.length--;
-  }
-}
-const myQueue = new Queue();
-myQueue.enqueue("John");
-myQueue.enqueue("Will");
-myQueue.enqueue("Rebeca");
-myQueue.enqueue("Michael");
-myQueue.dequeue();
-console.log(myQueue, "que");
+//     this.first = this.first.next;
+//     this.length--;
+//   }
+// }
+// const myQueue = new Queue();
+// myQueue.enqueue("John");
+// myQueue.enqueue("Will");
+// myQueue.enqueue("Rebeca");
+// myQueue.enqueue("Michael");
+// myQueue.dequeue();
+// console.log(myQueue, "que");
